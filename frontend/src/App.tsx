975d240966fb7ee5c3197fda3c2609868a677d89
bohongu <jaehong2782@naver.com>
2022-12-14 22:59:06 +0900

    setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,15 @@
-import React from 'react';
+import axios from 'axios';
 import { useQuery } from 'react-query';
 import styled from 'styled-components';
 
 const App = () => {
   async function testFetch() {
-    const response = await fetch(
+    const response = await axios.get(
       'https://pd674m4mc4.execute-api.ap-northeast-2.amazonaws.com/dev/test',
     );
-    return await response.text();
+    return response.data;
   }
+
   const { data, isLoading } = useQuery(['data'], testFetch);
   return (
     <Wrapper>
