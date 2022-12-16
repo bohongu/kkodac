22dbe00a917c5c179bb6f2d584d5029dfb242255
bohongu <jaehong2782@naver.com>
2022-12-16 22:12:56 +0900

    reset

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://pd674m4mc4.execute-api.ap-northeast-2.amazonaws.com';
+const BASE_URL = process.env.REACT_APP_API_URL;
 
 export const fetchTest = async () => {
   const response = await axios.get(`${BASE_URL}/dev/test`);
