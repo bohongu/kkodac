6774b10885ec8624c0cebdc91848bc8ac9e502fb
bohongu <jaehong2782@naver.com>
2022-12-16 22:16:40 +0900

    base url

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -3,6 +3,6 @@ import axios from 'axios';
 const BASE_URL = process.env.REACT_APP_API_URL;
 
 export const fetchTest = async () => {
-  const response = await axios.get(`${BASE_URL}/dev/test`);
+  const response = await axios.get(`${BASE_URL}/test`);
   return response.data;
 };
