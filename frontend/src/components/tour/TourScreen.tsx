4152c8c6dfbfa24fb58d9b55cae43599e6f0edb0
bohongu <jaehong2782@naver.com>
2023-01-01 20:09:47 +0900

    [#2] post hover

--- frontend/src/components/tour/TourScreen.tsx
+++ frontend/src/components/tour/TourScreen.tsx
@@ -1,6 +1,8 @@
+import { motion } from 'framer-motion';
 import React from 'react';
 import { Link, useMatch } from 'react-router-dom';
 import styled from 'styled-components';
+import { HoverDownVariants, ContentVariants } from '../../utils/variants';
 
 const TourScreen = () => {
   const match = useMatch('/tour/:region');
@@ -56,9 +58,17 @@ const TourScreen = () => {
         <button>스타일</button>
       </TagNav>
       <Posts>
-        <Post></Post>
-        <Post></Post>
-        <Post></Post>
+        <Post variants={HoverDownVariants} whileHover="hover">
+          <Content variants={ContentVariants}>
+            <h1>제목</h1>
+            <h2>작성자</h2>
+            <Tags>
+              <div>애월읍</div>
+              <div>카페</div>
+              <div>바닷가</div>
+            </Tags>
+          </Content>
+        </Post>
       </Posts>
     </TourWrapper>
   );
@@ -68,7 +78,7 @@ export default TourScreen;
 
 const TourWrapper = styled.div`
   ${(props) => props.theme.flex.flexCenterColumn}
-  margin: 0 20%;
+  margin: 0 15%;
   margin-top: 80px;
 `;
 
@@ -103,7 +113,41 @@ const Posts = styled.div`
   gap: 10px;
 `;
 
-const Post = styled.div`
+const Post = styled(motion.div)`
   border: 1px solid black;
   height: 300px;
+  background: white;
+  border-radius: 10px;
+`;
+
+const Content = styled(motion.div)`
+  border-top: 1px solid black;
+  position: relative;
+  top: 150px;
+  height: 150px;
+  opacity: 0;
+  padding: 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  h1 {
+    font-size: 20px;
+    margin-bottom: 10px;
+  }
+  h2 {
+    font-size: 12px;
+    margin-bottom: 10px;
+  }
+`;
+
+const Tags = styled.div`
+  display: flex;
+  div {
+    ${(props) => props.theme.flex.flexCenter}
+    border: 1px solid black;
+    width: 50px;
+    height: 20px;
+    font-size: 12px;
+    margin-right: 10px;
+  }
 `;
