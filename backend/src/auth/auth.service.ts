0b464e9e872a7c7b2b25b3c1a4989cf952b9f586
jihye-12 <cjstkwlcp1@naver.com>
2023-01-03 01:26:31 +0900

    login 1차

---
+++ backend/src/auth/auth.service.ts
@@ -0,0 +1,79 @@
+import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import CryptoJS from 'crypto-js';
+import { User } from 'src/user/entities/user.entity';
+import { getConnection } from 'typeorm';
+import { UsersService } from './users/users.service';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
+
+  async validateUser(user_email: string): Promise<any> {
+    const user = await this.usersService.findUserByEmail(user_email);
+    if (!user) {
+      return null;
+    }
+    return user;
+  }
+
+  async createLoginToken(user: User) {
+    const payload = {
+      user_no: user._id,
+      user_token: 'loginToken',
+    };
+
+    return this.jwtService.sign(payload, {
+      secret: process.env.JWT_SECRET,
+      expiresIn: '6m',
+    });
+  }
+
+  async createRefreshToken(user: User) {
+    const payload = {
+      user_no: user._id,
+      user_token: 'refreshToken',
+    };
+
+    const token = this.jwtService.sign(payload, {
+      secret: process.env.JWT_SECRET,
+      expiresIn: '50m',
+    });
+
+    const refresh_token = CryptoJS.AES.encrypt(
+      JSON.stringify(token),
+      process.env.AES_KEY,
+    ).toString();
+
+    await getConnection()
+      .createQueryBuilder()
+      .update(User)
+      .set({ user_refresh_token: token })
+      .where(`_id = ${user._id}`)
+      .execute();
+    return refresh_token;
+  }
+
+  onceToken(user_profile: any) {
+    const payload = {
+      user_email: user_profile.user_email,
+      user_nick: user_profile.user_nick,
+      user_provider: user_profile.user_provider,
+      user_token: 'onceToken',
+    };
+
+    return this.jwtService.sign(payload, {
+      secret: process.env.JWT_SECRET,
+      expiresIn: '10m',
+    });
+  }
+
+  async tokenValidate(token: string) {
+    return await this.jwtService.verify(token, {
+      secret: process.env.JWT_SECRET,
+    });
+  }
+}
---
+++ backend/src/auth/users/users.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import { getConnection } from 'typeorm';
+import { User } from './user.entity';
+
+@Injectable()
+export class UsersService {
+  async findUserByEmail(user_email: string): Promise<User | undefined> {
+    const user = await getConnection()
+      .createQueryBuilder()
+      .select('user')
+      .from(User, 'user')
+      .where('user.user_email = :user_email', { user_email })
+      .getOne();
+    return user;
+  }
+
+  async findUserById(user_no: number): Promise<User | undefined> {
+    const user = await getConnection()
+      .createQueryBuilder()
+      .select('user')
+      .from(User, 'user')
+      .where('user.user_no = :user_no', { user_no })
+      .getOne();
+    return user;
+  }
+}
